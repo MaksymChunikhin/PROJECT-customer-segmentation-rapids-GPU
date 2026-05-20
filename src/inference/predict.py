cfe30a0f1423b340583f97c56e619e2afdf1c481
Maksym Chunikhin <m.chunihin@gmail.com>
2026-05-20 18:43:46 +0200

    update

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -1,10 +1,14 @@
 import joblib
 import pandas as pd
 
+# load trained pipeline
 pipeline = joblib.load("../../outputs/models/final_pipeline.pkl")
 
 
 def predict_customer_segment(recency, frequency, monetary):
+    """
+    Predict customer segment using trained clustering pipeline.
+    """
 
     customer = pd.DataFrame({
         "Recency": [recency],
