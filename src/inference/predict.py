99d4b4357c8c01b137d140a28f0810a32c7f4d23
Maksym Chunikhin <m.chunihin@gmail.com>
2026-05-20 15:57:47 +0200

    Initial commit - PROJECT-customer-segmentation-rapids-GPU

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -1,25 +1,17 @@
 import joblib
 import pandas as pd
 
-
-# загрузка scaler и модели
-scaler = joblib.load("../../outputs/models/scaler.pkl")
-model = joblib.load("../../outputs/models/kmeans_model.pkl")
+pipeline = joblib.load("../../outputs/models/final_pipeline.pkl")
 
 
 def predict_customer_segment(recency, frequency, monetary):
 
-    # dataframe нового клиента
     customer = pd.DataFrame({
         "Recency": [recency],
         "Frequency": [frequency],
         "Monetary": [monetary]
     })
 
-    # scaling
-    customer_scaled = scaler.transform(customer)
-
-    # prediction
-    cluster = model.predict(customer_scaled)
+    cluster = pipeline.predict(customer)
 
     return int(cluster[0])
